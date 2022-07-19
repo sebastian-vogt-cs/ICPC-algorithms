cf5a3d1682513ef0c996af971042765b4cb34a2e
Sebastian Vogt <vogt@fim.uni-passau.de>
2022-07-19 20:19:18 +0200

    add floyd-warshall with doubles

---
+++ cpp/pathfinding/floyd-warshall.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+/**
+ * Calculates the shortest path between any two nodes in the input graph and inserts the shortest paths directly into
+ * the adjacency matrix.
+ *
+ * @param mat The adjacency matrix representing the graph to compute on, filled with double weight values.Must be a
+ *            n x n matrix. Nodes that are not connected must be represented by INFINITY.
+ * @param n The number of nodes in the graph.
+ */
+void floyd_double(vector<vector<double>>& mat, u32 n) {
+    for (u32 k = 0; k < n; k++) {
+        for (u32 i = 0; i < n; i++) {
+            for (u32 j = 0; j < n; j++) {
+                if (mat[i][k] + mat[k][j] < mat[i][j]) {
+                    mat[i][j] = mat[i][k] + mat[k][j];
+                }
+            }
+        }
+    }
+}
+
+template<typename T>
+void print_matrix(vector<vector<T>> const& matrix) {
+    for (vector<T> const& row: matrix) {
+        for (T const& n: row) {
+            cout << n << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    // Example for floyd_double:
+    vector<vector<double>> matrix{{INFINITY, 5.0, INFINITY},
+                                  {3.0, INFINITY, INFINITY},
+                                  {INFINITY, 2.0, INFINITY}};
+    floyd_double(matrix, 3);
+    print_matrix<double>(matrix);
+
+}
+
